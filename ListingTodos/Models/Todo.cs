73ed36ef970113649e55eb3da2574915d0de9b1a
r0nuh <cs.szilard@gmail.com>
2018-04-24 11:48:49 +0200

    feat(Models): use IdentityUser username and todo title is required

--- ListingTodos/Models/Todo.cs
+++ ListingTodos/Models/Todo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ListingTodos.Models
 {
     public class Todo
     {
         public long Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public bool IsUrgent { get; set; } = false;
         public bool IsDone { get; set; } = false;
--- ListingTodos/Models/User.cs
+++ ListingTodos/Models/User.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ListingTodos.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public long Id { get; set; }
-        public string UserName {get; set;}
+        public new long Id { get; set; }
+        [Required]
+        public override string UserName {get; set;}
+
         public string Name { get; set; }
 
         public List<Todo> Todos { get; set; }
